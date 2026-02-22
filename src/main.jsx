bc74900b58c048d825037b0650bc9e5d68f70e7f
Anna Desnenko <97408707+VioleetSky@users.noreply.github.com>
2026-02-23 01:45:28 +0200

    fix: added basename for routing and fixed data loading

--- src/main.jsx
+++ src/main.jsx
@@ -35,7 +35,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename="/MeTime">
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
                     <AppRouter />
